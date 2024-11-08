280429526a66fd54187b48cba51413500a151a06
Leod55 <l.dessanay@studenti.unica.it>
2024-11-08 15:51:31 +0100

    08/11/24 15:51

--- 1/mux/soluzione.ml
+++ 1/mux/soluzione.ml
@@ -8,4 +8,10 @@ let mux2 s0 a b = match s0 with
 
 
 
+let mux4 s0 s1 a0 a1 a2 a3 =  match (s0, s1) with 
+| (false, false) -> a0 
+| (false, true) -> a1
+| (true, false) -> a2
+| (true, true) -> a3;;
 
+let mux4 s0 s1 a0 a1 a2 a3 = if s0 then (mux2 s1 a3 a2) else (mux2 s1 a1 a0);;
